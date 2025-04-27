1389a5de4e448e1f5d710db238879ccd31c37d9f
Matej Farkaš <matejfarkas@hotmail.com>
2025-04-27 15:51:44 +0200

    fix prisma with workaround lib

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  /** Fix prisma client issue when deploying to Vercel */
+  webpack: (config, { isServer }) => {
+    if (isServer) config.plugins = [...config.plugins, new PrismaPlugin()];
+    return config;
+  },
+};
 
 module.exports = nextConfig;
