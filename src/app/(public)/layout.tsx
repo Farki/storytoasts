884a0f4f01a937df017576ec7a0ed3675e9bc6a4
Matej Farkaš <matejfarkas@hotmail.com>
2025-04-28 12:43:46 +0200

    mote to root

--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -7,16 +7,6 @@ export const metadata: Metadata = {
     default: "StoryToasts - Impactful Website Notifications",
     template: "%s | StoryToasts",
   },
-  description:
-    "Transform visitor engagement with attention-grabbing notifications that drive conversions.",
-  twitter: { card: "summary_large_image" },
-  openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: "https://storytoasts.com/",
-    siteName: "StoryToasts",
-    images: "/og-image.png",
-  },
 };
 
 export default function PublicLayout({
